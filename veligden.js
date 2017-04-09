7947f387ee06ca7874a8efe5d72a75513730d74e
nomce <3ccb45c5ab0f5c478416cd667b22294f8ae4c547>
2017-04-09 20:01:57 +0200

    posledniot veligden popraven

--- veligden.js
+++ veligden.js
@@ -7,7 +7,7 @@
           new Date(2019,3,21),new Date(2019,3,28),
           new Date(2020,3,12),new Date(2020,3,19),
           new Date(2021,3,4),new Date(2021,4,2),
-          new Date(2022,3,17),new Date(2018,3,24)];
+          new Date(2022,3,17),new Date(2022,3,24)];
      
         veligden.daliEDenesVeligden = function() {
             if (veligden.lista.find(compareDates) !== undefined){
